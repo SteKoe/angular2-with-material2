13ee8a45782f5a9727db077f36546afffa081c80
Stephan Köninger <github@stekoe.de>
2016-05-04 18:25:25 +0200

    chore(ng2-material): add material design

--- src/app/bc-typeahead.component.ts
+++ src/app/bc-typeahead.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {MdCheckbox} from '@angular2-material/checkbox';
 
 @Component({
   moduleId: module.id,
   selector: 'bc-typeahead-app',
   templateUrl: 'bc-typeahead.component.html',
+  directives: [MdCheckbox],
   styleUrls: ['bc-typeahead.component.css']
 })
 export class BcTypeaheadAppComponent {
